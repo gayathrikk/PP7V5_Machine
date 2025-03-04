3b536fcdaa203913b03d7ae476af25b8d159853e
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-03-04 12:53:22 +0530

    PP7V5.java

--- src/test/java/com/gayu/Gayathri/PP7V5.java
+++ src/test/java/com/gayu/Gayathri/PP7V5.java
@@ -109,9 +109,9 @@ public class PP7V5 {
 	   private void sendEmailAlert(String messageBody) {
 	        // Recipient's email ID needs to be mentioned.
 	    //  String[] to = {"annotation.divya@gmail.com"}; 
-	    	String[] to = {"karthik6595@gmail.com"};
+	    	String[] to = {"gayathrigayu0918@gmail.com"};
 	    	 // String[] cc = {"divya.d@htic.ittm.ac.in"};
-	       String[] cc = {"chinna02jobroi@gmail.com", "meerannagoor84@gmail.com", "sindhu.r@htic.iitm.ac.in", "nathan.i@htic.iitm.ac.in", "divya.d@htic.iitm.ac.in", "lavanyabotcha@htic.iitm.ac.in", "venip@htic.iitm.ac.in"};
+	       String[] cc = {"nathan.i@htic.iitm.ac.in", "divya.d@htic.iitm.ac.in", "venip@htic.iitm.ac.in"};
 	        String[] bcc = {};  	
 	      //  String[] to = {"karthik6595@gmail.com","annotation.divya@gmail.com", "gayathrigayu0918@gmail.com","nathan.i@htic.iitm.ac.in","venip@htic.iitm.ac.in", "lavanyabotcha@htic.iitm.ac.in"}; 
 	        // Sender's email ID needs to be mentioned
